e7ace072b9ae7407fa847145dcd6fda8d11a1584
jintao <>
2020-05-28 00:52:23 +0800

    fix

--- ARouter/ARouter/ARouter.swift
+++ ARouter/ARouter/ARouter.swift
@@ -9,15 +9,11 @@
 import Foundation
 
 public class ARouter: NSObject {
-    static private let _default = ARouter()
-
-    public class var `default`: AnyObject {
-        return ARouter._default as AnyObject
-    }
+    public static let `default`: AnyObject = ARouter()
     
     /*
         在函数调用时加上"?"，可以避免RunTime机制的的闪退
-        TODO: 总会有人不小心去掉"?"，然后又没有实现SEL... 该如何避免闪退呢?
+        TODO: 总会有人不小心去掉"?"，然后又没有实现SEL... Swift中该如何避免闪退呢?
      */
     public override class func resolveInstanceMethod(_ sel: Selector!) -> Bool {
         print("[ARouter]: SEL(\(NSStringFromSelector(sel))) Not Found")
