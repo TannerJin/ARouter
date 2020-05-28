3927608e85d5678bb2832703b03ab00fb3e954d8
jintao <>
2020-05-28 11:25:14 +0800

    fix

--- ARouter/ARouter/ARouter.swift
+++ ARouter/ARouter/ARouter.swift
@@ -11,18 +11,24 @@ import Foundation
 public class ARouter: NSObject {
     public static let `default`: AnyObject = ARouter()
     
+    private var lastForwardingImp: IMP?
+    
     public override class func resolveInstanceMethod(_ sel: Selector!) -> Bool {
         print("[ARouter]: SEL(\(NSStringFromSelector(sel))) Not Found")
         return super.resolveInstanceMethod(sel)
     }
     
     public override func forwardingTarget(for aSelector: Selector!) -> Any? {
-        let block: @convention(block) (AnyObject)->AnyObject? = { (objc) in
+        if let lastImp = lastForwardingImp {
+            imp_removeBlock(lastImp)
+        }
+        let block: @convention(block) (AnyObject, AnyObject)->AnyObject? = { (objc, _) in
             print("[ARouter]: SEL(\(NSStringFromSelector(aSelector))) had been forwardingTarget to \(type(of: objc))")
             return nil
         }
         let imp = imp_implementationWithBlock(block)
         class_addMethod(ARouterForwarding.self, aSelector, imp, nil)
+        lastForwardingImp = imp
         return ARouterForwarding()
     }
 }
